006dcf0ae1eaef9572e432b9cc0b5df314d6b245
stmakaes <marvin.kaesting@student.hs-anhalt.de>
2021-06-26 19:54:57 +0200

    First attempt on project structure

---
+++ www/src/components/GameBoard.tsx
@@ -0,0 +1,10 @@
+import {Component} from "react";
+
+/**
+ * Represents the board players move their pieces on.
+ */
+export class GameBoard extends Component<any, any> {
+    render() {
+        return undefined;
+    }
+}
