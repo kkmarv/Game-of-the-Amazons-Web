02fafdf297469e4b52f008206d808d5ecce88054
stmakaes <marvin.kaesting@student.hs-anhalt.de>
2021-09-17 00:58:40 +0200

    first component tests - added TimerInput.test.tsx - added AdvancedSettings.test.tsx - added snapshot folders to gitignore

--- www/src/components/AuthenticationScreen/AuthenticationScreen.tsx
+++ www/src/components/AuthenticationScreen/AuthenticationScreen.tsx
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import {RouteComponentProps, withRouter} from "react-router-dom";
 import AuthButton from "@hs-anhalt/auth-button";
+
 import "../../styles/components/_authentication-screen.scss"
 
 
@@ -19,7 +20,7 @@ class AuthenticationScreen extends Component<Props, State> {
             <div className={"auth-button"}>
                 <AuthButton
                     authServiceURL={"https://webengineering.ins.hs-anhalt.de:40989"}
-                    serviceBaseURLs={["https://webengineering.ins.hs-anhalt.de:40918"]}
+                    serviceBaseURLs={["https://webengineering.ins.hs-anhalt.de:40919"]}
                     onAuthorize={() => {
                         this.props.onAuthorize()
                         this.props.history.push("/lobby")
