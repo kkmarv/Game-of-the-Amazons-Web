2cb227e434bfd5b2f8b555b533490f59f6f92867
Wegener, Pascal <pascal.wegener@student.hs-anhalt.de>
2021-09-18 19:19:12 +0200

    added overflow again, (kinda) fixed logo position on desktop, resized arrows correctly mobile

--- www/src/components/LobbyScreen/Logo.tsx
+++ www/src/components/LobbyScreen/Logo.tsx
@@ -12,10 +12,8 @@ interface State {
 export class Logo extends Component<Props, State> {
     render() {
         return (
-            <div className={"logo btn"}>
-                <button onClick={this.props.onClick}>
-                    <img src={logo} alt={"Anhalt University Logo"}/>
-                </button>
+            <div className={"logo btn"} onClick={this.props.onClick}>
+                <img src={logo} alt={"Anhalt University Logo"}/>
             </div>
         )
     }
