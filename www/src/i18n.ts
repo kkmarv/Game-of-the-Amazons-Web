574626b8b9daea5e0b7200508ddb4cde3587af97
Wegener, Pascal <pascal.wegener@student.hs-anhalt.de>
2021-09-19 20:57:08 +0000

    Update i18n.ts

--- www/src/i18n.ts
+++ www/src/i18n.ts
@@ -9,7 +9,7 @@ i18n
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         debug: true,
-        fallbackLng: 'en',
+        fallbackLng: 'de',
         resources: {
             en: {
                 translation: {
